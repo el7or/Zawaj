e1136f0102791792099fe59f7a6d06bd1476eed0
Ahmed ElHor <zizooo.elhor@gmail.com>
2019-09-20 13:05:41 +0200

    remove unused pages

--- ZawajSPA/src/app/pages/pages.module.ts
+++ ZawajSPA/src/app/pages/pages.module.ts
@@ -5,7 +5,6 @@ import { NbMenuModule, NbAlertModule } from '@nebular/theme';
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 
@@ -15,7 +14,6 @@ import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
     ThemeModule,
     NbMenuModule,
     DashboardModule,
-    ECommerceModule,
     MiscellaneousModule,
     NbAlertModule,
     FormsModule,
