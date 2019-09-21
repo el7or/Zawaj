c23afad5a527bb42bc471609eb3cdc8f34a80d2a
Ahmed ElHor <zizooo.elhor@gmail.com>
2019-09-21 12:53:43 +0200

    localize title page

--- ZawajSPA/src/app/shared/services/langg.service.ts
+++ ZawajSPA/src/app/shared/services/langg.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Title } from '@angular/platform-browser';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +10,15 @@ export class LanggService {
   
   language = new BehaviorSubject<string>(localStorage.getItem('langg')=='en'?'en':'ar');
   lang = this.language.asObservable();
-  constructor() {
+  constructor(titleService: Title) {
     this.lang.subscribe(
       lang=>{
         if(lang == 'en'){
           localStorage.setItem('langg','en');
+          titleService.setTitle('Zawaj');
         }else{
           localStorage.setItem('langg','ar');
+          titleService.setTitle('موقع زواج');
         }
       }
     );
