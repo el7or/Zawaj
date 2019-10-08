6b3ec5e40fe135708ce3d5088561f658019e2ecf
Ahmed Elhor <zizooo.elhor@gmail.com>
2019-10-08 09:07:18 +0200

    clear app comp

--- ZawajSPA/src/app/app.component.ts
+++ ZawajSPA/src/app/app.component.ts
@@ -4,10 +4,8 @@ import { AuthService } from "./shared/services/auth.service";
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit, AfterViewChecked, OnDestroy } from "@angular/core";
+import { Component, OnInit, AfterViewChecked } from "@angular/core";
 import { Subscription } from "rxjs";
-// @ts-ignore
-import * as words from "../assets/locale/translation.json";
 import { LanggService } from "./shared/services/langg.service";
 import { JwtHelperService } from "@auth0/angular-jwt";
 
@@ -15,7 +13,7 @@ import { JwtHelperService } from "@auth0/angular-jwt";
   selector: "ngx-app",
   template: "<router-outlet></router-outlet>"
 })
-export class AppComponent implements OnInit, AfterViewChecked, OnDestroy {
+export class AppComponent implements OnInit, AfterViewChecked {
   _words = [];
   langgSubscription: Subscription;
   jwtHelper = new JwtHelperService();
@@ -26,8 +24,6 @@ export class AppComponent implements OnInit, AfterViewChecked, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this._words = words.default;
-
     const token = localStorage.getItem("token");
     const user = JSON.parse(localStorage.getItem("user"));
     if (token) {
@@ -40,53 +36,6 @@ export class AppComponent implements OnInit, AfterViewChecked, OnDestroy {
   }
 
   ngAfterViewChecked() {
-    let runTimeElements = document.querySelectorAll(
-      ".menu-title, div.message, span.title.subtitle, #swal2-title, #swal2-content, .swal2-confirm, nb-option.ng-star-inserted, span.info, span.tab-text, div.user-name"
-    );
-    let runTimeElementsArray = Array.prototype.slice.call(runTimeElements);
-    let lastElementsArray = this.langgService.elementsArray.value;
-    if (lastElementsArray.length != runTimeElementsArray.length) {
-      runTimeElementsArray.forEach(element => {
-        this.langgSubscription = this.langgService.lang.subscribe(
-          (lang: string) => {
-            try {
-              let words = this._words.filter(o =>
-                Object.keys(o).some(
-                  k => o[k] == element.innerText || o[k] == element.innerHTML
-                )
-              );
-              element.innerText = words[0][lang];
-            } catch {}
-          }
-        );
-      });
-      this.langgService.elementsArray.next(runTimeElementsArray);
-    }
-
-    let runTimePlacholders = document.querySelectorAll("input.search-input");
-    let runTimePlacholdersArray = Array.prototype.slice.call(
-      runTimePlacholders
-    );
-    let lastPlacholdersArray = this.langgService.PlacholdersArray.value;
-    if (lastPlacholdersArray.length != runTimePlacholdersArray.length) {
-      console.log(runTimePlacholdersArray);
-      runTimePlacholdersArray.forEach(element => {
-        this.langgSubscription = this.langgService.lang.subscribe(
-          (lang: string) => {
-            try {
-              let words = this._words.filter(o =>
-                Object.keys(o).some(k => o[k] == element.placeholder)
-              );
-              element.placeholder = words[0][lang];
-            } catch {}
-          }
-        );
-      });
-      this.langgService.PlacholdersArray.next(runTimePlacholdersArray);
-    }
-  }
-
-  ngOnDestroy() {
-    this.langgSubscription.unsubscribe();
+    this.langgService.translateWithoutDerctive();
   }
 }
--- ZawajSPA/src/app/shared/services/langg.service.ts
+++ ZawajSPA/src/app/shared/services/langg.service.ts
@@ -1,28 +1,74 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Title } from '@angular/platform-browser';
-
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { Title } from "@angular/platform-browser";
+// @ts-ignore
+import * as words from "../../../assets/locale/translation.json";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class LanggService {
-  
+  _words = [];
   elementsArray = new BehaviorSubject<[]>([]);
   PlacholdersArray = new BehaviorSubject<[]>([]);
-  language = new BehaviorSubject<string>(localStorage.getItem('langg')=='en'?'en':'ar');
+  language = new BehaviorSubject<string>(
+    localStorage.getItem("langg") == "en" ? "en" : "ar"
+  );
   lang = this.language.asObservable();
+
   constructor(titleService: Title) {
-    this.lang.subscribe(
-      lang=>{
-        if(lang == 'en'){
-          localStorage.setItem('langg','en');
-          titleService.setTitle('Zawaj');
-        }else{
-          localStorage.setItem('langg','ar');
-          titleService.setTitle('موقع زواج');
-        }
+    this.lang.subscribe(lang => {
+      if (lang == "en") {
+        localStorage.setItem("langg", "en");
+        titleService.setTitle("Zawaj");
+      } else {
+        localStorage.setItem("langg", "ar");
+        titleService.setTitle("موقع زواج");
       }
+    });
+  }
+
+  translateWithoutDerctive() {
+    this._words = words.default;
+    let runTimeElements = document.querySelectorAll(
+      ".menu-title, div.message, span.title.subtitle, #swal2-title, #swal2-content, .swal2-confirm, nb-option.ng-star-inserted, span.info, span.tab-text, div.user-name"
     );
-   }
+    let runTimeElementsArray = Array.prototype.slice.call(runTimeElements);
+    let lastElementsArray = this.elementsArray.value;
+    if (lastElementsArray.length != runTimeElementsArray.length) {
+      runTimeElementsArray.forEach(element => {
+        this.lang.subscribe((lang: string) => {
+          try {
+            let words = this._words.filter(o =>
+              Object.keys(o).some(
+                k => o[k] == element.innerText || o[k] == element.innerHTML
+              )
+            );
+            element.innerText = words[0][lang];
+          } catch {}
+        });
+      });
+      this.elementsArray.next(runTimeElementsArray);
+    }
+
+    let runTimePlacholders = document.querySelectorAll("input.search-input");
+    let runTimePlacholdersArray = Array.prototype.slice.call(
+      runTimePlacholders
+    );
+    let lastPlacholdersArray = this.PlacholdersArray.value;
+    if (lastPlacholdersArray.length != runTimePlacholdersArray.length) {
+      console.log(runTimePlacholdersArray);
+      runTimePlacholdersArray.forEach(element => {
+        this.lang.subscribe((lang: string) => {
+          try {
+            let words = this._words.filter(o =>
+              Object.keys(o).some(k => o[k] == element.placeholder)
+            );
+            element.placeholder = words[0][lang];
+          } catch {}
+        });
+      });
+      this.PlacholdersArray.next(runTimePlacholdersArray);
+    }
+  }
 }
