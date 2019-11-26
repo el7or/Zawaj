8ff4a9aa1f9b1845a623aad1a96bbcb87cac4133
Ahmed Elhor <zizooo.elhor@gmail.com>
2019-11-26 16:59:00 +0200

    add local notification - apk

--- zawaj-apk/src/app/pages/messages/messages.page.ts
+++ zawaj-apk/src/app/pages/messages/messages.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Plugins } from "@capacitor/core";
+const { LocalNotifications } = Plugins;
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.page.html',
-  styleUrls: ['./messages.page.scss'],
+  selector: "app-messages",
+  templateUrl: "./messages.page.html",
+  styleUrls: ["./messages.page.scss"]
 })
 export class MessagesPage implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    LocalNotifications.schedule({
+      notifications: [
+        {
+          title: "Test",
+          body: "Testttttttttt Testttttttttt Testttttttttt Testttttttttt Testttttttttt ",
+          id: 1,
+          schedule: { at: new Date(Date.now() + 1000 * 2) },
+          sound: null,
+          attachments: null,
+          actionTypeId: "",
+          extra: null,
+          //smallIcon: 'assets://icon/favicon.png'
+        }
+      ]
+    });
   }
-
 }
